579bc3cd34e9a01ef4c6fba6545d8981871c1824
sandeep gujjeti <bittusandeep074@gmail.com>
2026-04-01 13:37:31 +0530

    feat(inventory): develop inventory module

---
+++ inventory-service/src/main/java/com/vectorstore/inventory_service/entity/Inventory.java
@@ -0,0 +1,23 @@
+package com.vectorstore.inventory_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String skuCode;
+
+    private Integer quantity;
+}
