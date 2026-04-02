226323952afc72fb3e556ac6959437c33b908008
charanyama <mailto.charany@gmail.com>
2026-04-02 07:41:16 +0530

    fix(product): remove empty config file for product kafka producer

--- product-service/src/main/java/com/virtualstore/productservice/config/KafkaProducerConfig.java
+++
@@ -1,8 +0,0 @@
-package com.virtualstore.productservice.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class KafkaProducerConfig {
-    // Spring Boot auto-config is enough
-}
